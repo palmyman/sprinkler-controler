153d6c80608e7e9c51673371a1b3e9097c99e3bf
Lubomír Čuhel <cuhel.lubomir@gmail.com>
2013-10-17 11:48:22 +0200

    init

---
+++ src/sprinkler/controler/SprinklerControler.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sprinkler.controler;
+
+/**
+ *
+ * @author palmyman
+ */
+public class SprinklerControler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
